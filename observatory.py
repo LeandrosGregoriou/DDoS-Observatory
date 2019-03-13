d7f6651617c49b978a1e14fa3cd4682bef40519d
LeandrosGregoriou <25797507+LeandrosGregoriou@users.noreply.github.com>
2019-03-13 00:25:37 +0000

    Add files via upload

--- observatory.py
+++ observatory.py
@@ -11,40 +11,29 @@ import MySQLdb
 import sys
 
 
-
-
 class DBEditor(object):
 
 	def __init__(self, db):
 		self.db = db
 		self.cursor = db.cursor()
-
 	def insertRecord(self, prefix, prefixLength, asPath, BGPcommunities, 
 					nextHop, collector, timestamp):		
-		
 		query = "INSERT INTO recordsTable(prefix, prefixLength, asPath, \
 					BGPcommunities, nextHop, collector, timestamp)  \
 					VALUES (%s, %s, %s, %s, %s, %s, %s)" 
-				
 		args = (prefix, prefixLength, asPath, BGPcommunities, nextHop, collector, timestamp)
-		
 		self.cursor.execute(query, args)
-
 		self.db.commit()
 
-				
-
-
-
-db = MySQLdb.connect(host="localhost",    # your host, usually localhost
-						     user="root",         # your username
-						     passwd="password",  # your password
-						     db="RecordsDB")        # name of the data base
+db = MySQLdb.connect(host="localhost",          # host
+						     user="root",       # username
+						     passwd="password", # password
+						     db="RecordsDB")    # name of the data base
 db_editor = DBEditor(db)
 
 
 print '\n\n-----------------  Welcome to the DDoS Observatory  -----------------\n\n'
-
+	
 # Create a new bgpstream instance and a reusable bgprecord instance
 stream = BGPStream()
 rec = BGPRecord()
@@ -103,6 +92,7 @@ while not isValid:
 	except:
 		print "Try again!\n"
 '''
+
 # Time interval:
 stream.add_interval_filter(int(startTime),int(endTime))
 
@@ -114,30 +104,32 @@ print "|                         Record                       |                 
 print "----------------------------------------------------------------------------------------------------------------"			
 print "| Project | Collector |  Type  |    Time    |  Status  | Type |  Peer Address  | Peer ASN |       Prefix       |"
 
-#for building up purposes im only allowing 100 records to be collected
-
 # Get next record
 while(stream.get_next_record(rec)):
 	# Print the record information only if it is not a valid record
 	if rec.status != "valid":
 		#print "--INVALID RECORD--"
-		print '| {:6s} | {:9s} | {:6s} | {:10d} | {:8s} '.format(rec.project, rec.collector, rec.type, rec.time, rec.status)
+		print '|  {:6s} | {:9s} | {:6s} | {:10d} | {:8s} '.format(rec.project, rec.collector, rec.type, rec.time, rec.status)
 		#print "-----------------------------"
 	else:
 		elem = rec.get_next_elem()
 		while (elem):
-			#if it contains :, if it's a IPv6, then don't include it
-			#AND the word prefix is contained within the element fields
-			#AND only include records with larger than /24 prefixes
-			if ":" not in elem.peer_address and 'prefix' in elem.fields and int(elem.fields['prefix'].split("/")[1]) > 24:
-				# Print record and elem information
-				print '|  {:6s} | {:9s} | {:6s} | {:10d} | {:8s} '.format(rec.project, rec.collector, rec.type, rec.time, rec.status),
-				print '| {:4s} | {:14s} | {:8d} | {:18s} |'.format(elem.type, elem.peer_address, elem.peer_asn, elem.fields['prefix'])#, elem.fields, " |"
-				
-				db_editor.insertRecord(str(elem.fields['prefix']), elem.fields['prefix'].split("/")[1],\
-						str(elem.fields['as-path']), str(elem.fields['communities']), str(elem.fields['next-hop']), rec.collector, rec.time)
+			#make sure all needed keys in element are existant
+			if 'as-path' in elem.fields and 'prefix' and 'communities' and 'next-hop' in elem.fields:
+				communities = []
+				for c in elem.fields['communities']:
+					community = "{}:{}".format(c['asn'], c['value'])
+					communities.append(community)
+				#if it contains : it's a IPv6, then don't include it
+				#AND only include records with larger than /24 prefixes
+				if ":" not in elem.peer_address and int(elem.fields['prefix'].split("/")[1]) > 24:
+					# Print record and elem information
+					print '|  {:6s} | {:9s} | {:6s} | {:10d} | {:8s} '.format(rec.project, rec.collector, rec.type, rec.time, rec.status),
+					print '| {:4s} | {:14s} | {:8d} | {:18s} |'.format(elem.type, elem.peer_address, elem.peer_asn, elem.fields['prefix'])#, elem.fields, " |"
+					#add the record to the database
+					db_editor.insertRecord(str(elem.fields['prefix']), elem.fields['prefix'].split("/")[1],\
+							str(elem.fields['as-path']), str(elem.fields['communities']), str(elem.fields['next-hop']), rec.collector, rec.time)
 			elem = rec.get_next_elem()
 			
-
-print ("Completed logging 100 records.")
 db_editor.db.close()
+print("end of timeframe")
